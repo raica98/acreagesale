245dd17ed3fb1884c73ad274f1261294cfc7238a
raica98 <54879246+raica98@users.noreply.github.com>
2026-02-26 18:51:03 -0800

    Updated index.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
-import './tailwind.css'
+import '../tailwind.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
